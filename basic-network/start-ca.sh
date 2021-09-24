5ba392c98e0108c2fd705cdd071798ea5e50f194
joo-ju <rlawn97@gmail.com>
2021-09-25 01:23:28 +0900

    success enrollAdmin (~244p)

--- basic-network/start-ca.sh
+++ basic-network/start-ca.sh
@@ -4,7 +4,7 @@ set -ev
 docker-compose -f docker-compose-ca.yaml up -d ca.seller.ato.com
 
 sleep 1
-cd $GOPATH/src/ato/application/sdk
+cd $GOPATH/src/__ato/application/sdk
 node enrollAdmin.js
-sleep 1
-node registUsers.js
+# sleep 1
+# node registUsers.js
