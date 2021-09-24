7496810ed83b0a051d5802af94e547b568511960
joo-ju <rlawn97@gmail.com>
2021-09-24 15:02:49 +0900

    edit teardown.sh

--- basic-network/teardown.sh
+++ basic-network/teardown.sh
@@ -2,13 +2,14 @@
 set -ev
 
 # Shut down the Docker containers that might be currently running.
-docker-compose -f docker-compose-ca.yaml stop
+docker-compose -f docker-compose.yaml stop
+# docker-compose -f docker-compose-ca.yaml stop
 docker-compose stop
-docker-compose -f docker-compose-ca.yaml kill && docker-compose -f docker-compose-ca.yaml down --volumes --remove-orphans
+docker-compose -f docker-compose.yaml kill && docker-compose -f docker-compose.yaml down --volumes --remove-orphans
 docker-compose -f kill && docker-compose down --volumes --remove-orphans
 
 # remove the local state
-rm -rf $GOPATH/src/ato/application/wallet/
+# rm -rf $GOPATH/src/ato/application/wallet/
 
 # Your system is now clean
 docker stop $(docker ps -a -q)
